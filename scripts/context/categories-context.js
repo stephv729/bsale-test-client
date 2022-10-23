cb87e6533b8580b64a996ad665cf88ba6e87cfc9
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-23 14:59:05 -0500

    Change: fetchCategories to store first category id as default

--- scripts/context/categories-context.js
+++ scripts/context/categories-context.js
@@ -3,7 +3,7 @@ import { getCategories } from "../services/categories-services.js";
 async function fetchCategories() {
   const categories = await getCategories();
   this.categories = categories;
-  this.currentCategory = categories[0]
+  this.currentCategory = categories[0].id;
 }
 
 const categoriesProvider = {
