7941d5f471784f89816912c89f9135e31d199017
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-23 16:55:48 -0500

    Change: endpoint in products services

--- scripts/services/products-services.js
+++ scripts/services/products-services.js
@@ -1,5 +1,5 @@
 import apiFetch from "./api-fetch.js";
 
 export async function getProductsByCategory(category_id) {
-  return await apiFetch("/products_by_category/" + category_id);
+  return await apiFetch("/categories/" + category_id+ "/products");
 }
