043f4a0ae8602edf4554504be95e263ba959dc24
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-23 13:10:54 -0500

    Add: currentCategory to categories context

--- scripts/context/categories-context.js
+++ scripts/context/categories-context.js
@@ -3,10 +3,12 @@ import { getCategories } from "../services/categories-services.js";
 async function fetchCategories() {
   const categories = await getCategories();
   this.categories = categories;
+  this.currentCategory = categories[0]
 }
 
 const categoriesProvider = {
   categories: [],
+  currentCategory: null,
   fetchCategories,
 };
 
