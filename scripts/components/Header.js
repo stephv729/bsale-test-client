bdfe996afac7de950b5c2cf6716e57381c407b5c
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-22 21:05:25 -0500

    Add: header component

---
+++ scripts/components/Header.js
@@ -0,0 +1,11 @@
+function render() {
+  return `<span class="heading--lg">BSale Test Online Store</span>`;
+}
+
+const Header = {
+  toString() {
+    return render();
+  },
+};
+
+export default Header;
