3b372c547f6ad2961d36596877cc027d4d6927b1
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-22 20:41:03 -0500

    Remove: token name from config (no logging needed)

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-import { tokenKey } from "./scripts/config.js";
 import DOMHandler from "./scripts/dom-handler.js";
 
-const LoginPage = "<div>Login</div>"
-const HomePage = "<div>HomePage</div>"
 
 async function init() {
   try {
-    const token = sessionStorage.getItem(tokenKey);
-    if (!token) return DOMHandler.load(LoginPage);
     // await fetch products
     DOMHandler.load(HomePage);
   } catch (error) {
     console.log(error);
-    DOMHandler.load(LoginPage);
-    sessionStorage.removeItem(tokenKey);
   }
 }
 
--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,1 @@
-export const tokenKey = "bsale-test-client-token";
 export const BASE_URI = "https://bsale-test-api-production.up.railway.app";
