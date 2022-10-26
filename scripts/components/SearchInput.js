4d90dbea75a848f6654d04cf2931d4a0fa0322af
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-26 11:24:15 -0500

    Add: SearchInput listener

--- scripts/components/SearchInput.js
+++ scripts/components/SearchInput.js
@@ -1,19 +1,45 @@
 import productsProvider from "../context/products-context.js";
+import DOMHandler from "../dom-handler.js";
 
 function render() {
-  const { fecthProductsByName } = productsProvider;
   return `
-  <div>
-  <input/>
+  <div class="input-container">
+  <input placeholder="search" class="js-search-input"/>
+  <img src="assets/search-icon.svg" alt="search" class="right-icon">
   </div>
   `;
+}
+function search(query) {
+  if (!query) return;
+
+  productsProvider.status = "loading";
+  // DOMHandler.reload();
+  productsProvider
+    .fecthProductsByName(query)
+    .then(() => {
+      productsProvider.status = "success";
+      DOMHandler.reload();
+    })
+    .catch((error) => {
+      productsProvider.status = "error";
+      console.log(error);
+    });
+}
+
+function inputListener() {
+  const input = document.querySelector(".js-search-input");
+  input.addEventListener("input", (event) => {
+    search(event.target.value);
+  });
 }
 
 const SearchInput = {
   toString() {
     return render();
   },
-  addListeners() {},
+  addListeners() {
+    inputListener();
+  },
 };
 
 export default SearchInput;
