8c5e4217abb7b98f584f64ac8c3fde645efe0dd8
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-26 12:07:07 -0500

    Add: debounce search to listeners in SearchInput

--- scripts/components/SearchInput.js
+++ scripts/components/SearchInput.js
@@ -1,23 +1,31 @@
+import categoriesProvider from "../context/categories-context.js";
 import productsProvider from "../context/products-context.js";
 import DOMHandler from "../dom-handler.js";
+import { debounce } from "../utils.js";
 
 function render() {
+  const query = localStorage.getItem("query");
   return `
-  <div class="input-container">
-  <input placeholder="search" class="js-search-input"/>
-  <img src="assets/search-icon.svg" alt="search" class="right-icon">
+  <div class="inputs-wrapper">
+    <div class="input-container search-input">
+      <input placeholder="Search by name in all categories ..." class="js-search-input" value=${
+        query || ""
+      }>
+      <img src="assets/search-icon.svg" alt="search" class="right-icon">
+    </div>
+    <input type="select">
   </div>
   `;
 }
 function search(query) {
-  if (!query) return;
-
+  localStorage.setItem("query", query);
   productsProvider.status = "loading";
-  // DOMHandler.reload();
+  DOMHandler.reload();
   productsProvider
     .fecthProductsByName(query)
     .then(() => {
       productsProvider.status = "success";
+      categoriesProvider.currentCategory = null;
       DOMHandler.reload();
     })
     .catch((error) => {
@@ -28,8 +36,9 @@ function search(query) {
 
 function inputListener() {
   const input = document.querySelector(".js-search-input");
+  const debouncedOnInput = debounce(search, 2000);
   input.addEventListener("input", (event) => {
-    search(event.target.value);
+    debouncedOnInput(event.target.value);
   });
 }
 
