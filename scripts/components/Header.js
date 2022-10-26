4e22bf59094b43b7709af52f770ba8c799b85b46
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-26 14:28:15 -0500

    Add: logo listener in Header component

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -1,11 +1,30 @@
+import categoriesProvider from "../context/categories-context.js";
+import productsProvider from "../context/products-context.js";
+import DOMHandler from "../dom-handler.js";
 import SearchInput from "./SearchInput.js";
 
 function render() {
   return `<div class="header-container">
-  <span class="heading--md">BSale Test Online Store</span>
+  <span class="heading--md js-logo logo">BSale Test Online Store</span>
   ${SearchInput}
   </div>
   `;
+}
+function logoListener() {
+  const logo = document.querySelector(".js-logo");
+  logo.addEventListener("click", async (_event) => {
+    productsProvider.status = "loading";
+    DOMHandler.reload();
+    productsProvider.queryValue = "";
+    categoriesProvider.currentCategory = null;
+    try {
+      await productsProvider.fecthProducts();
+      DOMHandler.reload();
+    } catch (error) {
+      console.log(error);
+      DOMHandler.reload();
+    }
+  });
 }
 
 const Header = {
@@ -14,6 +33,7 @@ const Header = {
   },
   addListeners() {
     SearchInput.addListeners();
+    logoListener();
   },
 };
 
