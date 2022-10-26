9f7176a49639a414849b67500c4088825f3b9c35
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-26 10:34:41 -0500

    Add: getProductsByName to products-services

--- scripts/services/products-services.js
+++ scripts/services/products-services.js
@@ -1,5 +1,13 @@
 import apiFetch from "./api-fetch.js";
 
 export async function getProductsByCategory(category_id) {
-  return await apiFetch("/categories/" + category_id+ "/products");
+  return await apiFetch("/categories/" + category_id + "/products");
+}
+
+export async function getProductsByName(name) {
+  return apiFetch("/search", { method: "GET", body: { name: name } }).then(
+    (data) => {
+      return data;
+    }
+  );
 }
