ae50e696fe0f80789dfd5648dcee6362877ca28f
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-26 10:35:22 -0500

    Add: fetchProductsByName to products context

--- scripts/context/products-context.js
+++ scripts/context/products-context.js
@@ -1,15 +1,25 @@
 import { getProductsByCategory } from "../services/products-services.js";
 import categoriesProvider from "./categories-context.js";
 
-async function fecthProductsByCategories(){
-  const products = await getProductsByCategory(categoriesProvider.currentCategory)
-  this.products = products
-  this.status = "success"
+async function fecthProductsByCategories() {
+  const products = await getProductsByCategory(
+    categoriesProvider.currentCategory
+  );
+  this.products = products;
+  this.status = "success";
 }
+
+async function fecthProductsByName(query) {
+  const products = await getProductsByName(query);
+  this.products = products;
+  this.status = "success";
+}
+
 const productsProvider = {
   products: [],
   status: "idle",
   fecthProductsByCategories,
+  fecthProductsByName,
 };
 
 export default productsProvider;
